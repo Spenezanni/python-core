b3f32b3bff30fc10b65cb940acc6a9b7b10ee8a9
Spenezanni <gustavospenezanni@gmail.com>
2023-01-16 22:15:08 -0300

    Python Init

---
+++ proj1/calculadoraSoma.py
@@ -0,0 +1,7 @@
+num1 = input("Digite um numero: ")
+num2 = input("Digite outro numero para somar: ")
+
+numeroSoma = int(num1) + int(num2)
+
+print(numeroSoma)
+
---
+++ proj1/start2.py
@@ -0,0 +1,6 @@
+nome = input("Digite o seu nome: " )
+idade = input("Digite a sua idade: ")
+
+print("Ola " + nome + "! " + "Hoje você tem " + idade + " anos de idade!")
+
+
